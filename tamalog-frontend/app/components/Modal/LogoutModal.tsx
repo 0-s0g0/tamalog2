5a343094f8c2602de0fee38f40074dd347f8f50b
0-s0g0 <sogo.rei812@gmail.com>
2025-09-25 13:59:13 +0900

    delete:不要なインポート文の消去

--- tamalog-frontend/app/components/Modal/LogoutModal.tsx
+++ tamalog-frontend/app/components/Modal/LogoutModal.tsx
@@ -4,11 +4,8 @@
 
 //共通インポート
 import React from 'react';
-import { useState, useEffect } from 'react';
 import styles from './01Modal.module.css'; 
-import { Entry } from '../type';
-import { getFirestore, doc, setDoc, getDoc, arrayUnion } from 'firebase/firestore';
-import { db, auth } from '../../../firebase/firebase'
+
 
 interface LogoutModalProps {
   nickname: string;
@@ -23,10 +20,6 @@ export const LogoutModal: React.FC<LogoutModalProps> = ({
   setIsLogoutModalOpen,
   handleLogout,
 }) => {
-  // モーダルを閉じる
-  const closeModal = () => {
-    setIsLogoutModalOpen(false);
-  };
 
   return (
     <>
